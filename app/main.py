15d9e53a3b73b346f23ddea4d6b8ddc55cce38c7
Sergey Udachin <s.udachin@mail.ru>
2023-11-27 23:05:54 +0300

    feat: add useage check endpoint

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
+from app.models import User
+from app.models.user import UserAgeResponse
+
 app = FastAPI()
 
 
@@ -19,6 +22,12 @@ async def file():
 @app.get("/multiply")
 async def multiply(num1: int, num2: int):
     return {"result": num1 * num2}
+
+
+@app.post("/check_user_age")
+async def user(usr: User):
+    return UserAgeResponse(**usr.model_dump())
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True, workers=1)
