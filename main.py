6b8718ffd8c672d8d3bc511b5265cce11f1870f1
Sergey Udachin <s.udachin@mail.ru>
2023-11-27 21:06:20 +0300

    feat: responding with html file from `/file/` endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -6,3 +7,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
+
+
+@app.get("/file/")
+async def file():
+    return FileResponse("response.html")
