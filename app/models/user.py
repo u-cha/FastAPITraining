41db95f76f7d41e699f3154995723562bef012ef
Sergey Udachin <s.udachin@mail.ru>
2023-11-27 23:07:08 +0300

    feat: add `UserAgeResponse` model with computed_field annotation

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field
 
 
 class User(BaseModel):
-    id: int
     name: str
+    age: int
+
+
+class UserAgeResponse(User):
+    @computed_field(return_type=bool)
+    @property
+    def is_adult(self):
+        return self.age >= 18
