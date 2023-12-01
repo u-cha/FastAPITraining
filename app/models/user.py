dfd6da65f18025cf3be141a261682e23e13bd7eb
Sergey Udachin <s.udachin@mail.ru>
2023-12-01 17:54:27 +0300

    feat: update `User` model, add `UserCreate` model

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,12 @@
-from pydantic import BaseModel, computed_field
+import re
+from typing import Annotated, Optional
+
+from pydantic import BaseModel, computed_field, EmailStr, PositiveInt, Field
 
 
 class User(BaseModel):
     name: str
-    age: int
+    age: Optional[PositiveInt] = Field(default=None, gt=0, lt=100)
 
 
 class UserAgeResponse(User):
@@ -11,3 +14,8 @@ class UserAgeResponse(User):
     @property
     def is_adult(self):
         return self.age >= 18
+
+
+class UserCreate(User):
+    email: EmailStr
+    is_subscribed: bool = False
